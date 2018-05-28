145882fd73e93e012a931df77b42c4a66d1ba4c0
Cliffano Subagio <cliffano@gmail.com>
2018-05-28 23:40:12 +1000

    Fix secgroup IDs expectation to be an array.

--- test/cli.js
+++ test/cli.js
@@ -40,7 +40,7 @@ buster.testCase('cli - _aws', {
     this.mockProcess.expects('exit').once().withExactArgs(0);
     this.stub(OpenSesame.prototype, 'aws', function (region, secgroupIds, cb) {
       assert.equals(region, 'ap-southeast-2');
-      assert.equals(secgroupIds, 'sg-12345678');
+      assert.equals(secgroupIds, ['sg-12345678']);
       cb();
     });
     this.stub(bag, 'command', function (base, actions) {
