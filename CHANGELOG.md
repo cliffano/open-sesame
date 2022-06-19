c123b1d575e925cf44bfee994e649717a74863fd
Cliffano Subagio <cliffano@gmail.com>
2022-06-19 13:44:55 +1000

    +- Change module type to ESM +- Set min node engine to >= 16.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 ## Unreleased
 
 ### Changed
+- Change module type to ESM
+- Set min node engine to >= 16.0.0
 - Replace lint type from jshint to eslint
 - Replace coverage from buster-istanbul to c8
 - Replace doc type from dox-foundation to jsdoc
--- test/cli.js
+++ test/cli.js
@@ -1,33 +1,40 @@
-var bag          = require('bagofcli');
-var buster       = require('buster-node');
-var _cli         = require('bagofcli');
-var cli          = require('../lib/cli');
-const OpenSesame = require('../lib/opensesame');
-var referee      = require('referee');
-var assert       = referee.assert;
+"use strict"
+/* eslint no-unused-vars: 0 */
+import bag from 'bagofcli';
+import _cli from 'bagofcli';
+import cli from '../lib/cli.js';
+import fs from 'fs';
+import OpenSesame from '../lib/opensesame.js';
+import referee from '@sinonjs/referee';
+import sinon from 'sinon';
+const assert = referee.assert;
 
-buster.testCase('cli - exec', {
-  setUp: function () {
-    this.mock({});
-  },
-  'should contain commands with actions': function (done) {
-    var mockCommand = function (base, actions) {
-      assert.defined(base);
-      assert.defined(actions.commands.aws.action);
+describe('cli - exec', function() {
+  it('should contain commands with actions', function(done) {
+    const mockCommand = function (base, actions) {
+      assert.isString(base);
+      assert.isFunction(actions.commands.aws.action);
       done();
     };
-    this.stub(_cli, 'command', mockCommand);
+    sinon.stub(_cli, 'command').value(mockCommand);
     cli.exec();
-  }
+  });
 });
 
-buster.testCase('cli - _aws', {
-  setUp: function () {
-    this.mockCli = this.mock(_cli);
-    this.mockProcess = this.mock(process);
-  },
-  'should pass args as AWS options': function () {
-    var args = {
+describe('cli - _aws', function() {
+  beforeEach(function (done) {
+    this.mockCli = sinon.mock(_cli);
+    this.mockProcess = sinon.mock(process);
+    done();
+  });
+  afterEach(function (done) {
+    this.mockCli.verify();
+    this.mockProcess.verify();
+    sinon.restore();
+    done();
+  });
+  it('should pass args as AWS options', function() {
+    const args = {
       region: 'ap-southeast-2',
       secgroupId: 'sg-12345678',
       parent: {
@@ -38,14 +45,14 @@ buster.testCase('cli - _aws', {
       }
     };
     this.mockProcess.expects('exit').once().withExactArgs(0);
-    this.stub(OpenSesame.prototype, 'aws', function (region, secgroupId, cb) {
+    sinon.stub(OpenSesame.prototype, 'aws').value(function (region, secgroupId, cb) {
       assert.equals(region, 'ap-southeast-2');
       assert.equals(secgroupId, ['sg-12345678']);
       cb();
     });
-    this.stub(bag, 'command', function (base, actions) {
+    sinon.stub(bag, 'command').value(function (base, actions) {
       actions.commands.aws.action(args);
     });
     cli.exec();
-  }
+  });
 });
