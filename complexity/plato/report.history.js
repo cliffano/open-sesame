639db157b48945fa29ead7ad6ccb8b3f74c0b7ad
cliffano <cliffano@users.noreply.github.com>
2022-08-03 10:33:18 +0000

    deploy: 930de0e52e75fe92f50b92e37543c2511e06b51e

--- complexity/plato/report.history.js
+++ complexity/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 03 Aug 2022 10:32:13 GMT","total":{"sloc":217,"maintainability":143.951},"average":{"sloc":108,"maintainability":"71.98"}}]
+__history = [{"date":"Wed, 03 Aug 2022 10:33:10 GMT","total":{"sloc":217,"maintainability":143.951},"average":{"sloc":108,"maintainability":"71.98"}}]
