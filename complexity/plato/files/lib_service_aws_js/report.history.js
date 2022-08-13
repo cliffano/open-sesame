4dfcbc0fbf04b28a361ad6862efb4486bdc4d967
cliffano <cliffano@users.noreply.github.com>
2022-08-13 07:02:27 +0000

    deploy: 5155bf0bd0fef0255fa2a17b137efd778fb369c6

--- complexity/plato/files/lib_service_aws_js/report.history.js
+++ complexity/plato/files/lib_service_aws_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 13 Aug 2022 03:31:43 GMT","sloc":135,"lloc":72,"functions":11,"deliveredBugs":0.894,"difficulty":29.167,"maintainability":68.723,"lintErrors":11}]
+__history = [{"date":"Sat, 13 Aug 2022 07:02:21 GMT","sloc":135,"lloc":72,"functions":11,"deliveredBugs":0.894,"difficulty":29.167,"maintainability":68.723,"lintErrors":11}]
